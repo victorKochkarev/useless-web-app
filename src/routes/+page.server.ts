9b1039020fc29b8db4d2d2af03931aa71b8d762d
Victor Kochkarev <victor.kochkarev@gmail.com>
2024-06-29 13:09:43 +0100

    Added integration with Open AI

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,8 +1,22 @@
 import type { Actions } from './$types';
+import OpenAI from "openai";
 
 export const actions = {
 	callForAction: async ({ request }) => {
 		const formData = await request.formData();
-		console.log(formData)
+		let requestString = formData.get("actionText")
+
+		const openai = new OpenAI();
+
+		const prefix = "make an excuse why I should not";
+		const completion = await openai.chat.completions.create({
+			messages: [{ role: "system", content: prefix + " " + requestString}],
+			model: "gpt-3.5-turbo",
+		});
+
+		if(completion.choices.length == 0) {
+			return;
+		}
+		console.log(completion.choices[0].message.content)
 	},
 } satisfies Actions;
