63542b6ebeddd0933d326cf2f9c86089de0a9690
Victor Kochkarev <victor.kochkarev@gmail.com>
2024-06-29 11:50:12 +0100

    Added typography plugin.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
-<form method="POST" action="?/callForAction">
+<form method="POST" action="?/callForAction" class="prose">
 	<h1>What do you want me to do?</h1>
-	<textarea name="actionText"></textarea>
+	<textarea name="actionText"></textarea><br/>
 	<button class="btn btn-secondary" >Just Do it</button>
 </form>
 
